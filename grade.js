ee588be156096dcef86354b96a8b2a3256f59902
narmatha153 <narmatha153@gmail.com>
2025-01-07 12:43:52 +0530

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,13 @@
+var sid=parseInt(prompt("enter the id"));
+        var sname=prompt("enter the name")
+        var m1=parseInt(prompt("enter the m1"));
+        var m2=parseInt(prompt("enter the m2"));
+        var m3=parseInt(prompt("enter the m3"));
+        var m4=parseInt(prompt("enter the m4"));
+        var m5=parseInt(prompt("enter the m5"));
+        var total=m1+m2+m3+m4+m5;
+        var percentage=(total*100/500.0);
+        document.write("Student id=:"+sid+"<br>");
+        document.write("Student Name=:"+sname+"<br>");
+        document.write("Total=:"+total+"<br>");
+        document.write("Percentage=:"+percentage+"<br>");
